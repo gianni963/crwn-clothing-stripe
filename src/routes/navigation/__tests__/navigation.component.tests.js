1d41b885c82e1cec7efd5b525f22ea62250dc50e
Gianni Guatieri <gianniguatieri@Giannis-MacBook-Pro.local>
2025-07-27 00:28:31 +0200

    testing, Button tests, Render with Providers and Cart Icon Tests, Navigation Tests

---
+++ src/routes/navigation/__tests__/navigation.component.tests.js
@@ -0,0 +1,41 @@
+import { screen } from '@testing-library/react';
+
+import Navigation from '../navigation.component';
+import { renderWithProviders } from '../../../utils/test/test.utils';
+import { getCurrentUser } from '../../../utils/firebase/firebase.utils';
+
+describe('Navigation tests', () => {
+    test('It should render as Sign in link and not a Sign Out if there is no currentUser', () => {
+        renderWithProviders(<Navigation/>, {
+            preloadedState: {
+                user: {
+                    getCurrentUser: null,
+                }
+            }
+        });
+
+        const signInLinkElement = screen.getByText(/sign in/i);
+        expect(signInLinkElement).toBeInTheDocument();
+
+        const signOutLinkElement = screen.queryByText(/sign out/i);
+        expect(signOutLinkElement).toBeNull();
+    })
+
+    test('It should render Sign Out link and not Sign In link if there is a currentUser', () => {
+        renderWithProviders(<Navigation/>, {
+            preloadedState: {
+                user: {
+                    currentUser: {
+
+                    }
+                }
+            }
+        })
+
+        const signInLinkElement = screen.queryByText(/sign in/i);
+        expect(signInLinkElement).toBeNull();
+
+        const signOutLinkElement = screen.getByText(/sign out/i);
+        expect(signOutLinkElement).toBeInTheDocument();
+    });
+})
